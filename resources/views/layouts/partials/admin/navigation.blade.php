03f63801b8057b4b03ea1906084c532a25083023
ahuf <recibosalvaro@gmail.com>
2023-04-25 21:04:24 -0500

    126-Crear plantilla Admin

--- resources/views/layouts/partials/admin/navigation.blade.php
+++ resources/views/layouts/partials/admin/navigation.blade.php
@@ -1,36 +1,27 @@
-@php 
-    $links = [
-        [
-            'active'=> request()->routeIs('home') ,
-            'title' => 'Home',
-            'url'   => route('home'),
-        ]
-
-    ];
-@endphp
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav class="bg-white border-b border-gray-100 sticky top-0">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
-            <div class="flex">
+
+            <!-- Hamburger -->
+            <div class="-mr-2  items-center hidden lg:flex">
+                <button @click="openSidebar = ! openSidebar" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />                         
+                    </svg>
+                </button>
+            </div>
+
+            <div class="flex lg:hidden">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="/">
                         <x-application-mark class="block h-9 w-auto" />
                     </a>
-                </div>
-
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    @foreach ($links as $link)
-                        <x-nav-link href="{{ $link['url'] }}" :active=" $link['active']">
-                            {{ $link['title'] }}
-                        </x-nav-link>
-                    @endforeach
-                </div>
+                </div>            
             </div>
 
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
+            <div class="hidden lg:flex sm:items-center sm:ml-6">
                 <!-- Teams Dropdown -->
                 <!-- Settings Dropdown -->
                 @auth
@@ -96,7 +87,7 @@
             </div>
 
             <!-- Hamburger -->
-            <div class="-mr-2 flex items-center sm:hidden">
+            <div class="-mr-2 flex items-center lg:hidden ">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -108,7 +99,7 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+    <div :class="{'block': open, 'hidden': ! open}" class="hidden lg:hidden">
         <div class="pt-2 pb-3 space-y-1">
             @foreach ($links as $link)
                 <x-responsive-nav-link href="{{ $link['url'] }}" :active="$link['active']">
@@ -140,8 +131,7 @@
                         <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
                         <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
                     </div>
-                </div>
-            
+                </div>                    
             
                 <div class="mt-3 space-y-1">
                     <!-- Account Management -->
